a5281477054aef3961dc9094eae03addc1d2eb57
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-04-26 14:18:07 +0200

    add about page with photos

--- app/page.js
+++ app/page.js
@@ -1,15 +1,20 @@
-import { Inter } from "next/font/google";
+import { Cantarell, Inter, Montserrat } from "next/font/google";
 import styles from "./page.module.css";
 
-import Link from "next/link";
+//import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
+const cantarell = Cantarell({ 
+  weight: '700',
+  subsets: ["latin"] });
+
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div>
-        <h1>Sheila Aliaga</h1>
+        <h1 className={cantarell.className}>Sheila Aliaga</h1>
         <h2>WEB DEVELOPER</h2>
       </div>
     </main>
